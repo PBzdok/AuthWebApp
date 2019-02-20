71697c7a1dbc17a9a71b78158c26d4e186c8a196
PBzdok <philipp-bzdok@web.de>
2019-02-20 12:53:03 +0100

    Added message model with 1:x relationship with user; added field validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :messages
+  validates :name, presence: true
+  validates :email, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_111818) do
+ActiveRecord::Schema.define(version: 2019_02_20_114027) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
