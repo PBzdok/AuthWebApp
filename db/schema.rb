6f601b080f6fb40be602ae531e2c1061d0ebb5a4
PBzdok <philipp-bzdok@web.de>
2019-02-21 15:42:42 +0100

    persistent sessions with cookies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_21_120648) do
+ActiveRecord::Schema.define(version: 2019_02_21_135551) do
 
   create_table "messages", force: :cascade do |t|
     t.string "content"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_02_21_120648) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
