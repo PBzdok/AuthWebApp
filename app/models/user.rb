a37fa3b2fbe2c6a02b854b0f8e2376ad93b88235
PBzdok <philipp-bzdok@web.de>
2019-02-21 13:05:37 +0100

    downcase email saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :messages
 
+  before_save { self.email = email.downcase }
+
   validates :name, presence: true,
             length: { maximum: 50 }
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_114027) do
+ActiveRecord::Schema.define(version: 2019_02_21_120057) do
 
   create_table "messages", force: :cascade do |t|
     t.string "content"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_02_20_114027) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
