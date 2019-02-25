e099a038ee447f5c404098909cfdb6d0a1360eee
PBzdok <philipp-bzdok@web.de>
2019-02-25 15:06:22 +0100

    authorization for edit, update, destroy

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -34,20 +34,34 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get edit" do
+    log_in_as(@user)
     get edit_user_url(@user)
     assert_response :success
   end
 
   test "should update user" do
+    log_in_as(@user)
     patch user_url(@user), params: { user: { email: @user.email, name: @user.name, password: 'foobar'} }
     assert_redirected_to user_url(@user)
   end
 
   test "should destroy user" do
+    log_in_as(@user)
     assert_difference('User.count', -1) do
       delete user_url(@user)
     end
 
     assert_redirected_to users_url
+  end
+
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert_redirected_to login_url
   end
 end
